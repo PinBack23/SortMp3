bdc1980f316f0d9ee79617a1bcb71e30f20438e9
PinBack23 <pinback@outlook.de>
2016-12-27 13:46:51 +0100

    Initial checkin

---
+++ Options.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortMp3
+{
+    class Options
+    {
+        public Options()
+        {
+            this.NumberFormat = "000";
+        }
+
+        [Option('p', "path", Required = true, HelpText = "Path to Mp3's")]
+        public string Mp3Path { get; set; }
+
+        [Option('o', "outputpath", Required = true, HelpText = "Outputpath")]
+        public string OutputPath { get; set; }
+
+        [Option('f', "format", Required = false, HelpText = "Numberformat e.g. 000 -> 001 - 999")]
+        public string NumberFormat { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            HeadingInfo loHeadingInfo = new HeadingInfo("SortMp3");
+            HelpText loHelp = new HelpText(loHeadingInfo);
+            loHelp.Copyright = "Copyright © PinBack 2016";
+            loHelp.AddOptions(this);
+            return loHelp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortMp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loOptions = new Options();
+            string lsFullFileName;
+            int lnCounter = 1;
+            if (CommandLine.Parser.Default.ParseArguments(args, loOptions))
+            {
+                if (!Directory.Exists(loOptions.OutputPath))
+                    Directory.CreateDirectory(loOptions.OutputPath);
+                
+                foreach (var loFileInfo in ReadMp3Files(loOptions.Mp3Path).OrderBy(item => item.CreationTime))
+                {
+                    lsFullFileName = Path.Combine(loOptions.OutputPath, GetFileName(loFileInfo, lnCounter, loOptions));
+                    if (File.Exists(lsFullFileName))
+                        File.Delete(lsFullFileName);
+                    File.Copy(loFileInfo.FullName, lsFullFileName);
+                    lnCounter++;
+                }
+
+            }
+        }
+
+        private static List<FileInfo> ReadMp3Files(string psPath)
+        {
+            DirectoryInfo loDirectoryInfo = new DirectoryInfo(psPath);
+            return loDirectoryInfo.EnumerateFiles("*.mp3", SearchOption.AllDirectories).ToList();
+        }
+
+        private static string GetFileName(FileInfo poFileInfo, int pnCounter, Options poOptions)
+        {
+            string lsFormatString = "{0:" + poOptions.NumberFormat + "}_{1}";
+            return string.Format(lsFormatString, pnCounter, poFileInfo.Name);
+        }
+    }
+}
